18e29bb12e8b0175de8bb68151b635cd68f434ab
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-18 14:41:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-const myName = "Max";
+// const myName = "Max";
 
-console.log("Привіт котики!");
-console.log(myName);
-console.log(40 + 40);
-console.log("Good bye");
+// console.log("Привіт котики!");
+// console.log(myName);
+// console.log(40 + 40);
+// console.log("Good bye");
+
+const title = document.querySelector(".cats-title");
+console.log(title);
+
+title.textContent = "Hello Max";
