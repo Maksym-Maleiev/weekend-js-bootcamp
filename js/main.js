82dddbee4db5c1824ab51fdbafc192fcfb530a37
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-18 15:01:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,21 @@
 // console.log("Good bye");
 
 const title = document.querySelector(".cats-title");
-console.log(title);
 
-title.classList.remove('cats-title');
+const logotype = document.querySelector('.site-logo img');
+const body = document.querySelector('body');
 
-console.log(title);
+console.log(logotype);
+
+function onLogoClick() {
+  body.style.backgroundColor = '#0f3f3c';
+ };
+
+logotype.addEventListener('click', onLogoClick)
+
+// title.classList.remove('cats-title');
+
+// console.log(title);
 
 // title.style.cssText = "color: tomato; font-size: 50px";
 // title.style.fontSize = "42px";
